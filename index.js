57664ee0d0804e4aac15c844c77e105c6caac2cf
shimul705 <shimulgub@gmail.com>
2024-02-08 02:53:33 +0600

    function 2

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-function calculateMoney(ticketSale) {
-  let savings = 0;
-  if (0 <= ticketSale) {
-    savings = (ticketSale * 120) - (500 + (8 * 50));
-    return savings
-  }
+// function calculateMoney(ticketSale) {
+//   let savings = 0;
+//   if (0 <= ticketSale) {
+//     savings = (ticketSale * 120) - (500 + (8 * 50));
+//     return savings;
+//   }
 
+//   else {
+//     return 'Invalid Number! Please enter number above or equal 0';
+//   }
+// }
+
+// const result = calculateMoney(10);
+// console.log(result);
+
+function checkName(name) {
+  //write your code here
+  const nameCase = name.toLowerCase();
+  // console.log(nameCase);
+
+  const nameLastLetter = nameCase.slice(-1);
+  // console.log(nameLastLetter);
+  if (typeof name !== 'string') {
+    return "Invalid Input! Please Enter String only";
+  }
+  const nameCheckWith = ['a', 'y', 'e', 'i', 'o', 'u', 'w'];
+
+  if (nameCheckWith.includes(nameLastLetter)) {
+    return "It's a Good Name";
+  }
   else {
-    return 'Invalid Number! Please enter number above or equal 0'
+    return "It's a Bad Name";
   }
 }
-
-const result = calculateMoney(10);
-console.log(result);
+console.log(checkName("Shimul"));
