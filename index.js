50b5f5abf77433c63ac4472f301f6627732bc25f
shimul705 <shimulgub@gmail.com>
2024-02-08 01:54:04 +0600

    function 1

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function calculateMoney(ticketSale) {
-  if (0<=ticketSale) {
-    savings = (ticketSale * 120)-(500+(8*50));
+  let savings = 0;
+  if (0 <= ticketSale) {
+    savings = (ticketSale * 120) - (500 + (8 * 50));
     return savings
-    console.log("Baker vai er taka ache:" , savings);
-  }
-  else{
-    console.log("Invalid input");
   }
 
+  else {
+    return 'Invalid Number! Please enter number above or equal 0'
   }
+}
 
-  calculateMoney(10);
+const result = calculateMoney(10);
+console.log(result);
